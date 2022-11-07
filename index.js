19803fb803324d0af7597d63ca0e148d97701229
YingduoZhao <zhaoy283@newschool.edu>
2022-11-07 18:16:56 -0500

    secondcommit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hi!")
+
+// Import the express library and assign it to a variable
+const express = require('express')
+
+// Create an instance of an express application 
+const app = express()
+
+// Set the port the application will be running on
+const port = process.env.PORT || 3001
+
+// Set up a response for the root path of the application
+app.get('/', (req, res) => {
+  res.send("draw a killer whale🐋 killing a seal.")
+})
+
+// Set the application to listen a port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
